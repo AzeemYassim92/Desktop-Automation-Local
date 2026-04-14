0a6977ab8da749a44b445271b0d0d79ab273d809
Azeem Yassim <azeemyassim@gmail.com>
2026-04-13 20:28:14 -0400

    fixed startup issue

--- DesktopAutomationLab/DesktopAutomationLab/Services/ColorSamplerService.cs
+++ DesktopAutomationLab/DesktopAutomationLab/Services/ColorSamplerService.cs
@@ -5,27 +5,28 @@ namespace DesktopAutomationLab.Services;
 
 public class ColorSamplerService
 {
-    public ColorSample SampleAt(int x, int y)
+    public ColorSample SampleRegionCenter(RegionDefinition region)
     {
-        using var bitmap = new Bitmap(1, 1);
+        var width = Math.Max(region.Width, 1);
+        var height = Math.Max(region.Height, 1);
+
+        using var bitmap = new Bitmap(width, height);
         using var graphics = Graphics.FromImage(bitmap);
-        graphics.CopyFromScreen(x, y, 0, 0, new Size(1, 1));
-        var color = bitmap.GetPixel(0, 0);
+
+        graphics.CopyFromScreen(region.X, region.Y, 0, 0, new Size(width, height));
+
+        var centerX = width / 2;
+        var centerY = height / 2;
+
+        var pixel = bitmap.GetPixel(centerX, centerY);
 
         return new ColorSample
         {
-            X = x,
-            Y = y,
-            Red = color.R,
-            Green = color.G,
-            Blue = color.B
+            X = region.X + centerX,
+            Y = region.Y + centerY,
+            Red = pixel.R,
+            Green = pixel.G,
+            Blue = pixel.B
         };
-    }
-
-    public ColorSample SampleRegionCenter(RegionDefinition region)
-    {
-        var centerX = region.X + Math.Max(region.Width / 2, 0);
-        var centerY = region.Y + Math.Max(region.Height / 2, 0);
-        return SampleAt(centerX, centerY);
     }
 }
